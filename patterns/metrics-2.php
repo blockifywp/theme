8e8c0069273a4858dc1a5b81df2d1d2cfbfb9b53
seothemes <seothemeswp@gmail.com>
2022-07-20 00:28:54 +0800

    Change default duration of counter block

--- patterns/metrics-2.php
+++ patterns/metrics-2.php
@@ -8,7 +8,7 @@
 ?><!-- wp:columns {"align":"wide","style":{"spacing":{"padding":{"top":"2em","bottom":"2em"}}}} -->
 <div class="wp-block-columns alignwide alignwide" style="padding-top:2em;padding-bottom:2em"><!-- wp:column {"style":{"spacing":{"blockGap":"0px"}}} -->
 <div class="wp-block-column"><!-- wp:blockify/counter {"tag":"h6","suffix":"k","style":{"blockGap":"1em","spacing":{"blockGap":"0px"}},"layout":{"type":"flex","justifyContent":"center","orientation":"horizontal"},"fontSize":"72"} -->
-<h6 class="wp-block-blockify-counter has-72-font-size" data-start="0" data-end="100" data-duration="3" data-delay="0" data-suffix="k" tagname="h6" value="0">100</h6>
+<h6 class="wp-block-blockify-counter has-72-font-size" data-start="0" data-end="100" data-duration="2" data-delay="0" data-suffix="k" tagname="h6" value="0">100</h6>
 <!-- /wp:blockify/counter -->
 
 <!-- wp:paragraph {"align":"center","fontSize":"18"} -->
@@ -18,7 +18,7 @@
 
 <!-- wp:column {"style":{"spacing":{"blockGap":"0px"}}} -->
 <div class="wp-block-column"><!-- wp:blockify/counter {"end":"25","tag":"h6","suffix":"+","style":{"blockGap":"1em","spacing":{"blockGap":"0px"}},"layout":{"type":"flex","justifyContent":"center","orientation":"horizontal"},"fontSize":"72"} -->
-<h6 class="wp-block-blockify-counter has-72-font-size" data-start="0" data-end="25" data-duration="3" data-delay="0" data-suffix="+" tagname="h6" value="0">25</h6>
+<h6 class="wp-block-blockify-counter has-72-font-size" data-start="0" data-end="25" data-duration="2" data-delay="0" data-suffix="+" tagname="h6" value="0">25</h6>
 <!-- /wp:blockify/counter -->
 
 <!-- wp:paragraph {"align":"center","fontSize":"18"} -->
@@ -28,7 +28,7 @@
 
 <!-- wp:column {"style":{"spacing":{"blockGap":"0px"}}} -->
 <div class="wp-block-column"><!-- wp:blockify/counter {"end":"33","tag":"h6","suffix":"m","style":{"blockGap":"1em","spacing":{"blockGap":"0px"}},"layout":{"type":"flex","justifyContent":"center","orientation":"horizontal"},"fontSize":"72"} -->
-<h6 class="wp-block-blockify-counter has-72-font-size" data-start="0" data-end="33" data-duration="3" data-delay="0" data-suffix="m" tagname="h6" value="0">33</h6>
+<h6 class="wp-block-blockify-counter has-72-font-size" data-start="0" data-end="33" data-duration="2" data-delay="0" data-suffix="m" tagname="h6" value="0">33</h6>
 <!-- /wp:blockify/counter -->
 
 <!-- wp:paragraph {"align":"center","fontSize":"18"} -->
@@ -38,7 +38,7 @@
 
 <!-- wp:column {"style":{"spacing":{"blockGap":"0px"}}} -->
 <div class="wp-block-column"><!-- wp:blockify/counter {"end":"900","tag":"h6","style":{"blockGap":"1em","spacing":{"blockGap":"0px"}},"layout":{"type":"flex","justifyContent":"center","orientation":"horizontal"},"fontSize":"72"} -->
-<h6 class="wp-block-blockify-counter has-72-font-size" data-start="0" data-end="900" data-duration="3" data-delay="0" data-suffix="" tagname="h6" value="0">900</h6>
+<h6 class="wp-block-blockify-counter has-72-font-size" data-start="0" data-end="900" data-duration="2" data-delay="0" data-suffix="" tagname="h6" value="0">900</h6>
 <!-- /wp:blockify/counter -->
 
 <!-- wp:paragraph {"align":"center","fontSize":"18"} -->
