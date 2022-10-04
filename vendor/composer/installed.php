88dd1b26a244b68536401ea90d4e65ebdbbd0aca
seothemes <seothemeswp@gmail.com>
2022-10-04 22:21:34 +0800

    Add vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'name' => 'blockify/theme',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3eee4419a9a12941edf3cb6c3b420a62b581d09d',
+        'type' => 'wordpress-theme',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'blockify/theme' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3eee4419a9a12941edf3cb6c3b420a62b581d09d',
+            'type' => 'wordpress-theme',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wptrt/admin-notices' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '42101e8cf841b17342fcef984a06caf63990c6f2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wptrt/admin-notices',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
