dce8d19aa5d64c2518b1d0314a4184e22288cc2c
seothemes <seothemeswp@gmail.com>
2022-12-09 19:08:38 +0800

    Remove blocks

--- vendor/blockify/framework/assets/js/blocks/dark-mode-toggle/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'da99c0a27d256f7e7b47');
--- vendor/blockify/framework/assets/js/blocks/dark-mode-toggle/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'cbfd776ffc5a682977f9');
